e23398339f924ac40069e92e10150a832b9ecee9
OnlySarthak <devsarthak.hotline@gmail.com>
2026-01-13 01:48:38 +0530

    fix the cd part of yml file

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Server is running");
+  res.send("Server is running on 80...");
 });
 
 app.get("/health", (req, res) => {
