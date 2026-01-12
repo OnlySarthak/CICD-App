3e1e6822cf40e9c70a4b4b03b3971d26f2370a7b
OnlySarthak <devsarthak.hotline@gmail.com>
2026-01-12 17:42:07 +0530

    initial first version of app

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
+app.get("/health", (req, res) => {
+  res.status(200).json({ status: "ok" });
+});
+
+const PORT = 80;
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
+
+module.exports = app; // IMPORTANT for testing
