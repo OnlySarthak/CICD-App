4b441b435a701c3579373d3df06d6d0199ed2cbe
OnlySarthak <devsarthak.hotline@gmail.com>
2026-01-25 20:17:34 +0530

    changes3

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Server is running on 80...");
+  res.send("Server is running on 80....");
 });
 
 app.get("/health", (req, res) => {
@@ -15,7 +15,7 @@ app.get("/collage", (req, res) => {
 
 const PORT = 80;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server listening on port ${PORT}`);
 });
 
