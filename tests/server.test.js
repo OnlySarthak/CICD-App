8d0c09ad131a0b5ef048a24778ba994123b11dff
OnlySarthak <devsarthak.hotline@gmail.com>
2026-01-25 20:44:52 +0530

    changes 7

--- tests/server.test.js
+++ tests/server.test.js
@@ -1,5 +1,5 @@
 const request = require("supertest");
-const app = require("../app");
+const app = require('../server');
 
 describe("Server health check", () => {
   it("should respond on /health", async () => {
