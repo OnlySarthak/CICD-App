ae153fef2b00613d3c89bafee6aadbb14bd7caea
OnlySarthak <devsarthak.hotline@gmail.com>
2026-01-28 00:28:48 +0530

    update 4

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 const express = require("express");
 const app = express();
 
-app.use("/", (req, res) => {
-  res.send("Server is running on 80....");
-});
 
 app.use("/health", (req, res) => {
   res.status(200).json({ status: "ok fine 80₹" });
@@ -13,6 +10,9 @@ app.use("/collage", (req, res) => {
   res.status(200).json({ status: "ok" });
 });
 
+app.use("/", (req, res) => {
+  res.send("Server is running on 80....");
+});
 const PORT = 80;
 
 app.listen(PORT, "0.0.0.0", () => {
